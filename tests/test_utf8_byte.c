17ee85aecd7f79e22a38f284d482731efc45e359
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-09-02 03:19:08 -0400

    tests(feat): Add basic scaffolding to test byte api

--- tests/test_utf8_byte.c
+++ tests/test_utf8_byte.c
@@ -3,6 +3,19 @@
 #include "byte.h"
 #include "test.h"
 
-int main(void) {
+int test_suite_utf8_byte_count(void) {
     return 0;
+}
+
+int main(void) {
+    TestSuite suites[] = {
+        {"utf8_byte_count", test_suite_utf8_byte_count},
+    };
+    size_t count = sizeof(suites) / sizeof(TestSuite);
+
+    int result = 0;
+    for (size_t i = 0; i < count; i++) {
+        result |= test_suite_run(&suites[i]);
+    }
+    return result;
 }
