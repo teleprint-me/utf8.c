5851dc6d830746bf17389066aa0af1c749dd5e4a
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-09-02 20:08:16 -0400

    byte(feat): Add sketch for testing byte copying

--- tests/test_utf8_byte.c
+++ tests/test_utf8_byte.c
@@ -2,6 +2,8 @@
  * @file utf8/tests/test_utf8_byte.c
  */
 
+#include <stdlib.h>
+
 #include "byte.h"
 #include "test.h"
 
@@ -63,7 +65,7 @@ typedef struct TestUTF8ByteDiff {
     const uint8_t* payload;
     const ptrdiff_t start;
     const ptrdiff_t end;
-    ptrdiff_t expected;
+    const ptrdiff_t expected;
 } TestUTF8ByteDiff;
 
 int test_group_utf8_byte_diff(TestUnit* unit) {
@@ -88,7 +90,7 @@ int test_group_utf8_byte_diff(TestUnit* unit) {
 int test_suite_utf8_byte_diff(void) {
     TestUTF8ByteDiff data[] = {
         {"NULL start", NULL, 0, 1, -1},  // nul input
-        {"start == end", (uint8_t*) "abc", 0, 0, -1},  // no diff
+        {"start == end", (uint8_t*) "abc", 0, 0, 0},  // no diff
         {"end < start", (uint8_t*) "abc", 2, 1, -1},
         {"diff 0", (uint8_t*) "abc", 0, 0, 0},
         {"diff 2", (uint8_t*) "abc", 0, 2, 2},
@@ -111,10 +113,61 @@ int test_suite_utf8_byte_diff(void) {
     return test_group_run(&group);
 }
 
+typedef struct TestUTF8ByteCopy {
+    const char* label;
+    const uint8_t* payload;
+    const uint8_t* expected;
+} TestUTF8ByteCopy;
+
+int test_group_utf8_byte_copy(TestUnit* unit) {
+    TestUTF8ByteCopy* data = (TestUTF8ByteCopy*) unit->data;
+    uint8_t* actual = utf8_byte_copy(data->payload);
+
+    int64_t count = utf8_byte_count(data->payload);
+    if (-1 == count) {
+        return 1;
+    }
+
+    for (int64_t i = 0; i < count; i++) {
+        ASSERT_EQ(
+            actual[i],
+            data->payload[i],
+            "[TestUTF8ByteCopy] Failed: unit=%zu, payload='%s', expected=%c, got=%c",
+            unit->index,
+            (data->payload) ? (char*) data->payload : "NULL",
+            (char) data->expected[i],
+            (char) actual[i]
+        );
+    }
+
+    free(actual);
+    return 0;
+}
+
+int test_suite_utf8_byte_copy(void) {
+    TestUTF8ByteDiff data[] = {0};
+    size_t count = sizeof(data) / sizeof(TestUTF8ByteDiff);
+
+    TestUnit units[count];
+    for (size_t i = 0; i < count; i++) {
+        units[i].data = &data[i];
+    }
+
+    TestGroup group = {
+        .name = "utf8_byte_copy",
+        .count = count,
+        .units = units,
+        .run = test_group_utf8_byte_copy,
+    };
+
+    return test_group_run(&group);
+}
+
 int main(void) {
     TestSuite suites[] = {
         {"utf8_byte_count", test_suite_utf8_byte_count},
         {"utf8_byte_diff", test_suite_utf8_byte_diff},
+        {"utf8_byte_copy", test_suite_utf8_byte_copy},
     };
     size_t count = sizeof(suites) / sizeof(TestSuite);
 
